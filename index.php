b5ac5ad2040a0042a8de26a43dec6ada3b8312ae
coton_chen <coton.chen@gmail.com>
2015-10-22 19:22:12 +0800

    first version

---
+++ index.php
@@ -0,0 +1,13 @@
+
+<html lang="en">
+<head>
+
+</head>
+<body>
+
+<!--WeChat
+====================================================== -->
+<?php require_once "weChat/weChatShareJS.php"; ?>﻿
+</script>
+</body>
+</html>
